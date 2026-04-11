1a5c3cf41aa6f383a35230a0fbbe855d78b54b25
gosha <gosha.titov@icloud.com>
2026-04-12 01:45:50 +0300

    feat: add `TFSequence`, `TFOptionalSequence` and `TFSubsequence` type aliases

---
+++ Sources/Algorithm/Components/Sequence.swift
@@ -0,0 +1,8 @@
+/// A sequence of integer indeces.
+internal typealias TFSequence = [Int]
+
+/// A sequence of optional integer indices, representing possible matches.
+internal typealias TFOptionalSequence = [Int?]
+
+/// A strictly increasing sequence of integer indices (no `nil` values).
+internal typealias TFSubsequence = [Int]
