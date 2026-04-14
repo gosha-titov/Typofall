aad085b8857dc1568a20545f67ca7e01466868b2
gosha <gosha.titov@icloud.com>
2026-04-14 17:35:25 +0300

    chore: package update

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Typofall",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "Typofall",
